af62907fc00f8957f825a591baf2432d6b40c994
Johan Hanses <johanhanses@gmail.com>
2022-01-07 12:42:45 +0100

    dark mode toggler

---
+++ components/header/moon.js
@@ -0,0 +1,13 @@
+export default function Moon() {
+  return (
+    <svg
+      className="fill-current h-6 w-6 lg:h-8 lg:w-8"
+      viewBox="0 0 32 32"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="Layer_13" data-name="Layer 13">
+        <path d="m28.36 22.57a14 14 0 1 1 -16.06-20.07 1 1 0 0 1 1 1.68 11 11 0 0 0 7.7 18.82 10.86 10.86 0 0 0 5.94-1.74 1 1 0 0 1 1.42 1.31z" />
+      </g>
+    </svg>
+  )
+}
